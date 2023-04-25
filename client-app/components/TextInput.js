a25a85fac7c1da30c02389a08a9147e6503cee43
Jared Parker <parkerjjared@gmail.com>
2023-04-25 17:44:55 +0100

    fixed auto focus

--- client-app/components/TextInput.js
+++ client-app/components/TextInput.js
@@ -10,28 +10,38 @@ import styles from '@/styles/components/TextInput.module.scss';
 
 export default function TextInput({ name, type='text', defaultValue, children, innerRef, autoFocus, onChange, onSubmit, onBlur }){
 
-    const focused = useRef(false);
+    const autoFocused = useRef(false);
+    const inputEl = useRef(false);
+
+    const onKeyDown = ( e ) => {
+        if( e.keyCode === 13 ){
+            inputEl.current.blur();
+            if( onSubmit ) onSubmit(e);
+        }
+    }
 
     const ref = element => {
         if( element ){
-            if( !focused.current && autoFocus ){
-                focused.current = true;
+            inputEl.current = element;
+
+            if( !autoFocused.current && autoFocus ){
+                autoFocused.current = true;
                 element.focus();
-            }
-            onkeydown = e => {
-                if( e.keyCode === 13 ){
-                    element.blur();
-                    if( onSubmit ) onSubmit(e);
-                }
+                setTimeout( () => element.selectionStart = element.selectionEnd = 10000, 0 );
             }
         }
         if( innerRef ) innerRef.current = element;
     }
 
+    // Put cursor at end of input
+    if( inputEl.current && defaultValue && document.activeElement === inputEl.current ){
+        autoFocused.current = false;
+    }
+
     return (
         <LayoutGroup>
             <label className={styles.label}>{name}</label>
-            <input className={styles.input} type={type} placeholder={children} onChange={onChange} onBlur={onBlur} ref={ref} defaultValue={defaultValue}/>
+            <input className={styles.input} type={type} placeholder={children} onChange={onChange} onBlur={onBlur} onKeyDown={onKeyDown} ref={ref} defaultValue={defaultValue}/>
         </LayoutGroup>
     )
 }
